3be1bb3fbc65bb37865e12eb7d7b52fb3983dfc3
yasmi <yas1cf1@gmail.com>
2024-06-12 13:08:51 -0300

    haha

--- backend/src/main/java/com/bosch/example/DependenciesConfiguration.java
+++ backend/src/main/java/com/bosch/example/DependenciesConfiguration.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import com.bosch.example.impl.CollatzImplementation;
 import com.bosch.example.impl.ImaexpImplementation;
 import com.bosch.example.impl.PalindromoImplementation;
+import com.bosch.example.services.CollatzService;
 import com.bosch.example.services.ImaexpService;
 import com.bosch.example.services.PalindromoService;
 
@@ -22,6 +24,12 @@ public class DependenciesConfiguration {
     @Scope("singleton")
     public ImaexpService imaexpService() {
         return new ImaexpImplementation();
+    }
+
+    @Bean
+    @Scope("singleton")
+    public CollatzService collatzService() {
+        return new CollatzImplementation();
     }
 
 }
